cb35446d41c5c3c740a31a02b41329f7748b2c38
Valentin Todorov <valentin@todorov.at>
2025-10-14 14:19:49 +0200

    Add data sets

--- src/Robustbase.jl
+++ src/Robustbase.jl
@@ -1,7 +1,16 @@
 module Robustbase
 
+## Predefined datasets used in outlier detection literature
+include("data.jl")
+import .DataSets: phones, hbk, stackloss
+import .DataSets: weightloss, hs93randomdata, woodgravity
+import .DataSets: hills, softdrinkdelivery, animals
+
 greet() = print("Hello World from Robustbase!")
 
 export greet
+
+## Data
+export hbk, stackloss, wood, animals
 
 end
